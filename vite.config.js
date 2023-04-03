04b8289c00efe68338ad42ebcf18d786765b21fa
Jonathan Manes <manesjona@gmail.com>
2023-04-03 16:36:17 +0200

    PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Cogip',
+        short_name: 'Cogip Web Client',
+        start_url: './',
+        display: 'standalone',
+        background_color: '#ffffff',
+        lang: 'fr',
+        scope: './',
+        icons: [
+          {
+            src: '/vite.svg',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: '/vite.svg',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+        theme_color: '#ffffff',
+      },
+    }),
+  ],
 })
